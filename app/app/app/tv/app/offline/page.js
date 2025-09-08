0b9ed7967fa17bbc74a9b2306d98437bbd595551
patricklazzarintruth <patrick@truthcommerce.com.br>
2025-09-08 20:09:31 -0300

    Create page.js

---
+++ app/app/app/tv/app/offline/page.js
@@ -0,0 +1,8 @@
+export default function Offline() {
+  return (
+    <main style={{padding:16}}>
+      <h1>Você está offline</h1>
+      <p>As ações serão enviadas quando a conexão voltar.</p>
+    </main>
+  );
+}
