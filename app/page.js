ac0d618eca495888301d38b2cdb6a49a959af54d
patricklazzarintruth <patrick@truthcommerce.com.br>
2025-09-08 20:55:58 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Gestão à Vista ✅</h1>
+      <p>Rota “/” ok. Se você está vendo isso, o deploy está funcionando.</p>
+    </main>
+  );
+}
